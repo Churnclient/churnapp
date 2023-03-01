02580d2b30d20818d88321e057a5c10e0a7c9404
Churnclient <126669691+Churnclient@users.noreply.github.com>
2023-03-01 11:50:32 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,74 @@
+import pickle
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.preprocessing import StandardScaler
+
+# Load the model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Define the features
+features = ["CreditScore", "Geography_France", "Geography_Spain", "Geography_Germany", "Gender_Male", "Gender_Female", "Age", "Tenure", "Balance", "NumOfProducts", "HasCrCard", "IsActiveMember", "EstimatedSalary"]
+
+# Define the StandardScaler
+scaler = StandardScaler()
+
+# Define the input fields
+st.write("# Client Churn Prediction")
+credit_score = st.number_input("Credit Score", value=650)
+geography = st.radio("Geography", options=["France", "Spain", "Germany"])
+gender = st.radio("Gender", options=["Male", "Female"])
+age = st.number_input("Age", value=35)
+tenure = st.number_input("Tenure", value=5)
+balance = st.number_input("Balance", value=100000)
+num_of_products = st.number_input("Number of Products", value=2)
+has_cr_card = st.radio("Has Credit Card", options=["Yes", "No"])
+is_active_member = st.radio("Is Active Member", options=["Yes", "No"])
+estimated_salary = st.number_input("Estimated Salary", value=100000)
+
+# Prepare the input data for prediction
+input_data = np.array([[credit_score]])
+
+# Encode the categorical variables
+if geography == "France":
+    input_data = np.append(input_data, [1, 0, 0])
+elif geography == "Spain":
+    input_data = np.append(input_data, [0, 1, 0])
+else:
+    input_data = np.append(input_data, [0, 0, 1])
+
+if gender == "Male":
+    input_data = np.append(input_data, [1, 0])
+else:
+    input_data = np.append(input_data, [0, 1])
+
+input_data = np.append(input_data, [age, tenure, balance, num_of_products])
+
+if has_cr_card == "Yes":
+    input_data = np.append(input_data, [1])
+else:
+    input_data = np.append(input_data, [0])
+
+if is_active_member == "Yes":
+    input_data = np.append(input_data, [1])
+else:
+    input_data = np.append(input_data, [0])
+
+input_data = np.append(input_data, [estimated_salary])
+
+# Scale the input data
+input_data = scaler.fit_transform(input_data.reshape(1, -1))
+
+# Make a prediction
+prediction = model.predict(input_data)
+
+# Display the prediction
+st.write("## Prediction")
+if prediction == 0:
+    st.write("The client is predicted to stay.")
+    st.balloons()
+else:
+    st.write("The client is predicted to churn.")
+    st.snow()
+st.stop()
